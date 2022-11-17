2b243400e3f3b1606b052c1e1505d8b78e10bc2f
Diaa-E <blarb21@yahoo.com>
2022-11-18 01:23:23 +0200

    Fix Random date generation

--- src/dev.utility.js
+++ src/dev.utility.js
@@ -38,7 +38,7 @@ export function generateRandomTasks(projects, maxTasks)
     {    
         for (let i = 0; i < Math.ceil(Math.random() * maxTasks); i++)
         {
-            projects[j].addTask((Task(generateRandomText(10), generateRandomText(20), generateRandomProjects(10), 2)));
+            projects[j].addTask((Task(generateRandomText(10), generateRandomText(20), generateRandomText(10), 2)));
         }
     }
 }
